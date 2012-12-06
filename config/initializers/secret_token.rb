7c419986dfcbda4c57e754450c77df6632b719e6
Lyomin Dmitriy <leminds@gmail.com>
2012-12-06 12:32:24 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Snoutbook::Application.config.secret_token = 'd663265cc1b099e0db54fc997313fee797cb8c2c1d519453d59134cf15c53dc0fc549c8fc3e27d42ef7d2cc18fd1569d544076a8d7c138b364c65afbed0ffa53'
